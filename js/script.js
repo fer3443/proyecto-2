7be35fba4600cf8b4a472f4d9b9a9951e6448158
Fernando <fer3443@gmail.com>
2023-01-05 10:53:32 -0300

    const de form creadas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 if(document.title ==="Formulario"){
     const inputNombre= document.querySelector("#nombre")
     const inputApellido= document.querySelector("#apellido")
+    const inputEdad= document.querySelector("#edad")
+    const inputMail= document.querySelector("#user")
+    const inputTelefono= document.querySelector("#telefono")
+    const inputCalle= document.querySelector("#calle")
+    const inputNumero= document.querySelector("#numero")
+    const inputCiudad= document.querySelector("#ciudad")
+    const inputSexo= document.querySelector("input[name=sexo]")
+    const boton= document.querySelector("#botonRegistro")
+    let personal= []
+    
+    if(localStorage.getItem("personal")){
+        personal= JSON.parse(localStorage.getItem("personal"))
+    }
 }
